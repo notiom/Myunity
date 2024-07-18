a230476b7e3277d97656a406616bf295d21c10ba
notiom <99580052+notiom@users.noreply.github.com>
2024-07-18 15:13:31 +0800

    Update FollowTarget.cs

--- 吃球游戏/FollowTarget.cs
+++ 吃球游戏/FollowTarget.cs
@@ -1,1 +1,19 @@
-das
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    private Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
